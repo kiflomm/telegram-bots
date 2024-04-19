4e2a18bf1fa55f4fff6aa32a636891a093f40984
Kiflom Berihu Abay <kiflomberihu2024@gmail.com>
2024-04-19 06:46:40 +0000

    The birth day teller bot is alive now

--- birth_day_teller/index.js
+++ birth_day_teller/index.js
@@ -2,32 +2,33 @@ const {Bot} = require("grammy")
 const express = require('express')
 const app = express()
 require('dotenv').config()
-botToken = process.env.BOT_TOKEN
+botToken = process.env.BOT_TOKEN || process.env.BOT_TOKEN_DEV
 PORT = process.env.PORT || 5000
 const bot = new Bot(botToken) 
 app.get('/',(req,res)=>{ 
     res.json({message: "the bot is live"})
 })
-app.listen(PORT,console.log(`server is listening to port ${PORT}`))
+app.listen(PORT,console.log(`The bot is alive`))
 
 bot.command('start',async (ctx) =>{
-    const {first_name} = ctx.message.from
-    console.log(`A new user ${first_name} has subscribed for this bot`) 
+    const {first_name} = ctx.message.from 
     await ctx.reply("Please enter your birth date in the format YYYY-MM-DD")
 })
-bot.on('message',async (ctx) =>{
+bot.on('message:text',async (ctx) =>{
     const userId = ctx.from.id
     const {first_name} = ctx.message.from
     const birthDateInput = ctx.message.text
     const birthDate = new Date(birthDateInput)
     if(!isNaN(birthDate.getTime())){
         let days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
-        let dayIndex = birthDate.getDay()
-        console.log(`The user was born on ${days[dayIndex]}`)
+        let dayIndex = birthDate.getDay() 
         ctx.reply(`You were born on ${days[dayIndex]}`)
     }else{
         ctx.reply("Please enter the date format in the YYYY-MM--DD format")
     }
 })  
+bot.on('message:photo',async (ctx)=>{
+    await ctx.reply("You sent us nice pic thank you")
+})
 bot.start()
 module.exports = app
